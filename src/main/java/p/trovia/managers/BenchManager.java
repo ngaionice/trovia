13f67d7e099e0b41fa5b5fc839d91a680e5f3b91
Julian <julian.myne@gmail.com>
2020-12-31 00:29:32 -0500

    Forgot to actually add BenchManager

---
+++ src/main/java/p/trovia/managers/BenchManager.java
@@ -0,0 +1,71 @@
+package p.trovia.managers;
+
+import p.trovia.objects.Bench;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BenchManager implements Manager, Serializable {
+
+    // use rPath as the key, as it is unique to each Bench
+    Map<String, Bench> benchMap = new HashMap<>(5000);
+
+    // used to track changes between local version and online database:
+    Map<String, Bench> addMap = new HashMap<>(1000);
+    Map<String, Bench> removeMap = new HashMap<>(1000);
+
+    // addMap is used to track new and edited benches
+    // removeMap is used to track removed benches (relative to last serialized/synchronization state)
+
+    public void addRecipe(Bench bench) {
+        benchMap.put(bench.getRPath(), bench);
+        addMap.put(bench.getRPath(), bench);
+    }
+
+    public void removeRecipe(String rPath) {
+        removeMap.put(rPath, benchMap.get(rPath));
+        benchMap.remove(rPath);
+    }
+
+    // getters
+
+    public String getName(String rPath) {
+        return benchMap.get(rPath).getName();
+    }
+
+    public List<String> getAllRecipes(String rPath) {
+        return benchMap.get(rPath).getAllRecipes();
+    }
+
+    public Map<String[], List<String>> getAllRecipesByCategory(String rPath) {
+        return benchMap.get(rPath).getAllRecipesByCategory();
+    }
+
+    /**
+     * Returns the associated profession name for the specified bench, if applicable.
+     * Returns null otherwise.
+     *
+     * @param rPath the relative path of the bench
+     * @return the associated profession name
+     */
+    public String getProfessionName(String rPath) {
+        if (benchMap.get(rPath).isProfession()) {
+            return benchMap.get(rPath).getProfessionName();
+        }
+        return null;
+    }
+
+    // setters
+
+    /**
+     * Sets the profession name identifier of the specified bench.
+     *
+     * @param rPath the relative path of the bench
+     * @param name the identifier of the name (in a LangFile)
+     */
+    public void setProfessionName(String rPath, String name) {
+        benchMap.get(rPath).setProfessionName(name);
+    }
+}
